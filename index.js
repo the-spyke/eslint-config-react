a525910d008dbdf60c77adcf8cbef15136ad25b2
Anton Alexandrenok <antonalexandrenok@gmail.com>
2017-05-01 14:36:28 +0300

    Added initial configuration

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": [
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "jsx-quotes": "error",
+        "react/no-string-refs": "error"
+    }
+}
